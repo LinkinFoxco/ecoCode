51fa23b5e7f60303d49aa0dc9d4fe608fe2a54de
Tristan ROULET <tristan.roulet@viacesi.fr>
2022-01-25 11:57:18 +0100

    Auto-completion of VibratorFreeRule because of the extend FlagOnMethodCheck

--- sonarqube-plugin-greenit/native-analyzer/android-plugin/src/main/java/io/ecocode/java/checks/sobriety/VibrationFreeRule.java
+++ sonarqube-plugin-greenit/native-analyzer/android-plugin/src/main/java/io/ecocode/java/checks/sobriety/VibrationFreeRule.java
@@ -1,4 +1,24 @@
 package io.ecocode.java.checks.sobriety;
 
-public class VibrationFreeRule {
+import io.ecocode.java.checks.helpers.constant.FlagOnMethodCheck;
+import org.sonar.check.Rule;
+
+@Rule(key = "ESOB011", name = "ecocodeVibrationFree")
+public class VibrationFreeRule extends FlagOnMethodCheck {
+    /**
+     * Constructor to configure the rule on a given class and method.
+     *
+     * @param methodName           name of the method to check
+     * @param methodOwnerType      name of the type that own the method
+     * @param constantValueToCheck the constant value to check
+     * @param paramPositions       the position(s) of the argument on the method to check
+     */
+    protected VibrationFreeRule(String methodName, String methodOwnerType, int constantValueToCheck, int... paramPositions) {
+        super(methodName, methodOwnerType, constantValueToCheck, paramPositions);
+    }
+
+    @Override
+    public String getMessage() {
+        return "Prefer not using VIBRATOR_SERVICE in API 26";
+    }
 }
